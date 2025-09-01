fc220d9697a45dddded8063d184ea01895b00780
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2025-09-01 20:05:45 -0300

    update

--- script/Level0Solution.s.sol
+++ script/Level0Solution.s.sol
@@ -1,4 +1,5 @@
-//SPDX: License-Identifier: MIT
+//SPDX:License-Identifier: MIT
+
 pragma solidity ^0.8.13;
 
 import "../src/Level0.sol";
@@ -7,14 +8,15 @@ import "../lib/forge-std/src/Script.sol";
 import "../lib/forge-std/src/console.sol";
 
 contract Level0Solution is Script {
-   
-   Level0 public level0 = Level0(0x9cc4c0ebCE7A8637C3a4c210edB60Bb995c87481);
+
+   Level0 public leve0Instance = Level0(0x9cc4c0ebCE7A8637C3a4c210edB60Bb995c87481);
 
    function run() external {
-    string memory password = level0.password();
-    console.log("Password: ", password);
-    vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-    level0.authenticate(password);
-    vm.stopBroadcast();
+      string memory password = leve0Instance.password();
+      console.log("Password: ", password);
+      vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+      leve0Instance.authenticate(password);
+      vm.stopBroadcast();
    }
+
 }
